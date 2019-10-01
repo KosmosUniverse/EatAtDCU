06e26ebde8eb2a94150a75f3cefa0a2366b5babb
thomas <thomas.ducret@epitech.eu>
2019-10-01 14:43:21 +0200

    add db and display restaurants with infos

--- src/eatatdcu/views.py
+++ src/eatatdcu/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Restaurant,Campus
@@ -7,7 +8,17 @@ def index(request):
    return render(request,'eatatdcu/index.html',context)
 
 def restaurants(request):
-   context = {}
+   context = {'campus':None, 'restaurant':None}
+
+   campus = request.GET.get('campus').lower()
+
+   campusList = Campus.objects.filter(name=campus)
+   if (len(campusList) >= 1):
+      context["campus"] = campus
+      restList = Restaurant.objects.filter(campus_id=campusList[0].campus_id)
+      if (len(restList) >= 1):
+         context["restaurant"] = restList
+      
 
    #TODO get the campus name from the request
    #TODO retrieve the campus id from the db given this campus name
